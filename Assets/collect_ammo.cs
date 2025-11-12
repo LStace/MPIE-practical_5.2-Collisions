8a6ed60ecb3791f68ea18e88dfb58f1a12f21c6c
Lee Stacey <155361420+LStace@users.noreply.github.com>
2025-11-12 10:55:36 +0000

    Added sfx

--- Assets/collect_ammo.cs
+++ Assets/collect_ammo.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class collect_ammo : MonoBehaviour
 {
+    public AudioClip GunEmptySFX;
+    public AudioClip GunShotSFX;
+    public AudioClip AmmoSFX;
+    public AudioClip GunReloadSFX;
     int ammo = 0;
+    AudioSource GunSFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GunSFX = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -14,6 +19,7 @@ public class collect_ammo : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1")){
             Shoot();
+            Reload();
         }
     }
 
@@ -22,14 +28,20 @@ public class collect_ammo : MonoBehaviour
             ammo += 20;
             Debug.Log(ammo);
             other.gameObject.SetActive(false);
+            GunSFX.clip = AmmoSFX;
+            GunSFX.Play();
         }
     }
 
     void Shoot(){
         if (ammo <= 0){
-            Debug.Log("No");
+            Debug.Log("no");
+            GunSFX.clip = GunEmptySFX;
+            GunSFX.Play();
             return;
         }
+        GunSFX.clip = GunShotSFX;
+        GunSFX.Play();
 
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f ,0.5f ,0.0f));
 
@@ -47,8 +59,17 @@ public class collect_ammo : MonoBehaviour
         if (g.name == "Target"){
             Animation animation = g.transform.parent.GetComponent<Animation>();
             animation.Play("LowerBridge");
+            AudioSource loweringSFX = g.transform.parent.GetComponent<AudioSource>();
+            loweringSFX.Play();
         } 
+    }
 
-        
+    void Reload(){
+        if (ammo == 0){
+            return;
+        }
+        yield return new WaitForSecondsRealTime(2);
+        GunSFX.clip = GunReloadSFX;
+        GunSFX.Play();
     }
 }
--- Assets/door.cs
+++ Assets/door.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class door : MonoBehaviour
 {
     Animation animation;
+    AudioSource DoorSFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameObject parent = transform.parent.gameObject;
         animation = parent.GetComponent<Animation>();
+        DoorSFX = GetComponent<AudioSource>();
         
     }
 
@@ -19,9 +21,11 @@ public class door : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
         animation.Play("OpenDoor");
+        DoorSFX.Play();
     }
 
     void OnTriggerExit(Collider other){
         animation.Play("Close");
+        DoorSFX.Play();
     }
 }
